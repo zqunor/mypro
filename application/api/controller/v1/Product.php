7e8892614a88364ef9b91d077be74a9a83102670
zhouqun <zqunor@gmail.com>
2018-06-28 19:44:41 +0800

    8-22分类商品接口

--- application/api/controller/v1/Product.php
+++ application/api/controller/v1/Product.php
@@ -5,6 +5,7 @@ namespace app\api\controller\v1;
 use think\Controller;
 use app\api\validate\Count;
 use app\lib\exception\ProductMissException;
+use app\api\validate\IDMustPositiveInt;
 
 class Product extends Controller
 {
@@ -19,6 +20,26 @@ class Product extends Controller
         // 在database.php中配置之后，不需要手动转换为collection
         // $productCollection = collection($products);
         // $products = $productCollection->hidden(['summary']);
+        $products = $products->hidden(['summary']);
+        return $products;
+    }
+
+    /**
+     * 通过分类id获取分类下的所有商品
+     *
+     * @param [int] $id 商品分类id
+     * @return void
+     */
+    public function getAllInCategory($id)
+    {
+        (new IDMustPositiveInt())->goCheck();
+
+        $products = model('Product')->getProductsByCategoryId($id);
+
+        if ($products->isEmpty()) {
+            throw new ProductMissException();
+        }
+
         $products = $products->hidden(['summary']);
         return $products;
     }
